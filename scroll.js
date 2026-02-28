ad09986f3e170262b123d7db6947273215691e67
mazn74793-png <mazn74793@gmail.com>
2026-02-28 17:11:14 +0200

    Create scroll.js

---
+++ scroll.js
@@ -0,0 +1,8 @@
+const items = document.querySelectorAll(".reveal");
+const io = new IntersectionObserver((entries) => {
+  entries.forEach((e) => {
+    if (e.isIntersecting) e.target.classList.add("show");
+  });
+}, { threshold: 0.12 });
+
+items.forEach((el) => io.observe(el));
